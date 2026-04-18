0b18516030e8f389a8f3ce7a5de234896a9da9cd
Victoria Beauray Sagady <cephypi@pm.me>
2026-04-18 11:16:34 -0500

    dev: setup linters, removed tailwind

--- apps/client/src/main.rs
+++ apps/client/src/main.rs
@@ -1,9 +1,10 @@
+mod components;
 use dioxus::prelude::*;
 
 const ICON: Asset = asset!("/icons/icon.ico");
 const LOGO: Asset = asset!("/assets/undertone_icon_transparent.svg");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+const MAIN_CSS: Asset = asset!("/assets/style/main.css");
+// const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
     tracing_subscriber::fmt::init();
@@ -16,28 +17,11 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: ICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        span { "hello" }
-        Hero {}
+        // document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
-    }
-}
-
-#[component]
-pub fn Hero() -> Element {
-    rsx! {
-        div { id: "hero",
-            img { src: LOGO, id: "header", class: "w-[30cqi]" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
-                    "💫 VSCode Extension"
-                }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+        div{
+            class: "w-[50cqi] m-auto",
+            img { src: LOGO}
         }
     }
 }
