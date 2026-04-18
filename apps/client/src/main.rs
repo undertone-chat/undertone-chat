0ba197d285bd8563bce37e4828a9b4af42707c8e
Victoria Beauray Sagady <cephypi@pm.me>
2026-04-17 21:41:23 -0500

    work: getting dioxus app up and running

--- apps/client/src/main.rs
+++ apps/client/src/main.rs
@@ -1,19 +1,23 @@
 use dioxus::prelude::*;
 
-const FAVICON: Asset = asset!("/assets/favicon.ico");
+const ICON: Asset = asset!("/icons/icon.ico");
+const LOGO: Asset = asset!("/assets/undertone_icon_transparent.svg");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
+    tracing_subscriber::fmt::init();
+    tracing::info!("Launching dioxus app");
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
     rsx! {
-        document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        document::Link { rel: "icon", href: ICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        span { "hello" }
         Hero {}
 
     }
@@ -22,15 +26,16 @@ fn App() -> Element {
 #[component]
 pub fn Hero() -> Element {
     rsx! {
-        div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
+        div { id: "hero",
+            img { src: LOGO, id: "header", class: "w-[30cqi]" }
             div { id: "links",
                 a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
                 a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
                 a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
                 a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
+                    "💫 VSCode Extension"
+                }
                 a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
             }
         }
--- docs/roadmap.md
+++ docs/roadmap.md
@@ -1,22 +1,16 @@
-# Undertone Development Roadmap
+![Undertone](undertone_icon_transparent_256x256.png)
+
+# Development Roadmap
 
 This file shall act as a living document of the roadmap for [Undertone](../README.md) and its associated projects.
 
-## 0.1 -> 1.0
+- [ ] TCP Control Layer - RPC and Commands to manage ssessions and connections.
+- [ ] User Sessions - State management of users on server and client
+- [ ] UDP Data Layer - Transport audio and real time associated data like player positions on the wire.
+- [ ] Channels - Allow server members to join and leave voice channels.
+- [ ] Administration - Server authorative admin controls through client to allow configuration and management.
+- [ ] Data Persistence - Serverside flat file database (plain text readable)
+- [ ] Layer Encryption - E2E encryption for data being transmitted between server and clients (Optional with variation for level of security vs overhead)
+- [ ] LUA Scripting Engine - Allow users to create scripts server and client side that can interpret data and modify packet metadata for users.
 
-- [ ] Basic connection between server and client.
-- [ ] TCP/IP Control Layer Foundation
-    - [ ] KeepAlive
-    - [ ] CommandAck
-    - [ ] Handshake
-- [ ] UDP Voice Layer Foundation
-    - [ ] Frame
-    - [ ] Header
-    - [ ] Ordering
-    - [ ] Error Handling
-    - [ ] Jitter
-- [ ] Client Audio
-    - [ ] Cross Platform Audio Playback
-    - [ ] Cross Platform Audio Capture
-    - [ ] Cross Platofrm Keybinds
 
