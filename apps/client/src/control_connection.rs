dcc0ee932309e3c2ce2d917744bf894b4d44a7af
CephyPi <cephypi@pm.me>
2026-04-20 19:02:08 -0500

    feat: proper control packet handling with rkyv

--- apps/client/src/control_connection.rs
+++ apps/client/src/control_connection.rs
@@ -1,9 +1,12 @@
 use crossbeam_channel::Sender as CrossbeamSender;
+use dioxus::hooks::UnboundedReceiver;
 use dioxus::prelude::Signal;
 use dioxus::signals::WritableExt;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
+use undertone_common::AlignedVec;
+use undertone_common::protocol::control::{ControlPacket, decode_packet, encode_packet};
 
 use crate::audio::AudioCommand;
 use crate::ui_events::UiEvent;
@@ -11,136 +14,162 @@ use crate::ui_events::UiEvent;
 #[derive(Debug)]
 pub enum ControlCommand {
     Connect(String),
-    Join(u32),
     Disconnect,
+    Send(ControlPacket),
 }
 
-#[derive(Debug)]
+enum SessionCommand {
+    Send(ControlPacket),
+    Shutdown,
+}
+
+struct SessionHandle {
+    tx: tokio::sync::mpsc::Sender<SessionCommand>,
+    task: tokio::task::JoinHandle<()>,
+}
+
 pub struct ControlConnection {
     cmd_rx: UnboundedReceiver<ControlCommand>,
     event_tx: UnboundedSender<UiEvent>,
-    audio_tx: CrossbeamSender<AudioCommand>,
-    stream: Option<TcpStream>,
+    audio_tx: crossbeam_channel::Sender<AudioCommand>,
+    session: Option<SessionHandle>,
+    next_request_id: u64,
 }
 
 impl ControlConnection {
     pub fn new(
-        mut cmd_rx: UnboundedReceiver<ControlCommand>,
+        cmd_rx: UnboundedReceiver<ControlCommand>,
         event_tx: UnboundedSender<UiEvent>,
-        audio_tx: CrossbeamSender<AudioCommand>,
+        audio_tx: crossbeam_channel::Sender<AudioCommand>,
     ) -> Self {
         Self {
             cmd_rx,
             event_tx,
             audio_tx,
-            stream: None,
+            session: None,
+            next_request_id: 1,
         }
     }
 
-    pub async fn run(&mut self) {
-        // let mut stream: Option<TcpStream> = None;
-
+    pub async fn run(mut self) {
         while let Some(cmd) = self.cmd_rx.recv().await {
             match cmd {
-                ControlCommand::Connect(address) => {
-                    if self.stream.is_some() {
-                        tracing::warn!("Already connected!");
-                        return;
-                    }
-                    let _ = self
-                        .event_tx
-                        .send(UiEvent::ConnectionStatus("Connecting...".into()));
-                    match TcpStream::connect(&address).await {
-                        Ok(tcp) => {
-                            let _ = self
-                                .event_tx
-                                .send(UiEvent::ConnectionStatus("Connected!".into()));
-                            self.stream = Some(tcp);
-                            // Pass stream to handler in new thread.
-                            tokio::spawn(async move {
-                                stream_handler(tcp).await;
-                            });
-                            let _ = self.audio_tx.send(AudioCommand::StartStreaming(address));
-                        }
-                        Err(error) => {
-                            let _ = self
-                                .event_tx
-                                .send(UiEvent::ConnectionStatus(format!("Error: {}", error)));
-                        }
-                    }
-                }
-                ControlCommand::Disconnect => {
-                    self.stream = None;
-                    let _ = self.event_tx.send(UiEvent::ConnectionLost);
-                    let _ = self.audio_tx.send(AudioCommand::Shutdown);
-                }
-                any => {
-                    tracing::debug!("Unhandled command: {:?}", any);
-                }
+                ControlCommand::Connect(addr) => self.connect(addr).await,
+                ControlCommand::Disconnect => self.disconnect().await,
+                ControlCommand::Send(packet) => self.send(packet).await,
+            }
+        }
+
+        self.disconnect().await;
+    }
+
+    async fn connect(&mut self, addr: String) {
+        if self.session.is_some() {
+            let _ = self
+                .event_tx
+                .send(UiEvent::ConnectionStatus("Already Connected".into()));
+            return;
+        }
+
+        let _ = self
+            .event_tx
+            .send(UiEvent::ConnectionStatus("Connecting...".into()));
+
+        match TcpStream::connect(&addr).await {
+            Ok(stream) => {
+                let (tx, rx) = mpsc::channel(64);
+
+                let event_tx = self.event_tx.clone();
+                let audio_tx = self.audio_tx.clone();
+
+                let task = tokio::spawn(async move {
+                    run_session(stream, rx, event_tx, audio_tx).await;
+                });
+                self.session = Some(SessionHandle { tx, task });
+
+                let _ = self
+                    .event_tx
+                    .send(UiEvent::ConnectionStatus("Connected!".into()));
+            }
+            Err(error) => {
+                let _ = self
+                    .event_tx
+                    .send(UiEvent::ConnectionStatus(format!("Error: {error}")));
             }
         }
     }
-}
 
-async fn stream_handler(mut socket: TcpStream) {
-    // let Some(mut socket) = stream else {
-    //     return;
-    // };
+    async fn disconnect(&mut self) {
+        todo!();
+    }
 
-    let mut buf = [0; 1024];
-    tracing::debug!(
-        "stream  handler started for connection to {}",
-        socket.peer_addr().unwrap()
-    );
-
-    loop {
-        socket.readable().await.unwrap();
-        let n = match socket.read(&mut buf).await {
-            Ok(0) => return,
-            Ok(n) => n,
-            Err(error) => {
-                tracing::error!("Failed to read from socket; err= {:?}", error);
-                return;
-            }
-        };
-
-        let answer = std::str::from_utf8(&buf[0..n]).expect("some utf bs");
-        tracing::debug!("Got: {}", answer);
+    async fn send(&mut self, packet: ControlPacket) {
+        todo!();
     }
 }
 
-// pub async fn run_tcp_actor(
-//     mut cmd_rx: UnboundedReceiver<ControlCommand>,
-//     event_tx: UnboundedSender<UiEvent>, // UI Listener
-//     audio_tx: CrossbeamSender<AudioCommand>,
-// ) {
-//     let mut stream: Option<TcpStream> = None;
-//
-//     // This loop stays alive independently of the UI components
-//     while let Some(cmd) = cmd_rx.recv().await {
-//         match cmd {
-//             ControlCommand::Connect(address) => {
-//                 tracing::debug!("Got Control Command Connect");
-//                 let _ = event_tx.send(UiEvent::ConnectionStatus("Connecing...".into()));
-//                 match TcpStream::connect(&address).await {
-//                     Ok(tcp) => {
-//                         let _ = event_tx.send(UiEvent::ConnectionStatus("Connected!".into()));
-//                         stream = Some(tcp);
-//                         let _ = audio_tx.send(AudioCommand::StartStreaming(address));
-//                     }
-//                     Err(e) => {
-//                         let _ = event_tx.send(UiEvent::ConnectionStatus(format!("Error: {}", e)));
-//                     }
-//                 }
-//             }
-//             ControlCommand::Disconnect => {
-//                 stream = None;
-//                 let _ = event_tx.send(UiEvent::ConnectionLost);
-//                 let _ = audio_tx.send(AudioCommand::Shutdown);
-//             }
-//             _ => {
-//                 tracing::debug!("Unhandled Command Message!");
-//             }
-//         }
-//     }
-// }
+async fn run_session(
+    stream: TcpStream,
+    mut rx: mpsc::Receiver<SessionCommand>,
+    event_tx: UnboundedSender<UiEvent>,
+    audio_tx: crossbeam_channel::Sender<AudioCommand>,
+) {
+    let peer = stream.peer_addr().ok();
+    let (mut reader, mut writer) = stream.into_split();
+
+    let reader_task = async {
+        loop {
+            match read_packet(&mut reader).await {
+                Ok(packet) => {
+                    handle_inbound_packet(packet, &event_tx, &audio_tx).await;
+                }
+                Err(error) => {
+                    tracing::warn!(?peer, ?error, "control reader ended");
+                    break;
+                }
+            }
+        }
+    };
+
+    let writer_task = async {
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                SessionCommand::Send(packet) => {
+                    if let Err(error) = write_packet(&mut writer, &packet).await {
+                        tracing::warn!(?peer, ?error, "control writer ended");
+                        break;
+                    }
+                }
+                SessionCommand::Shutdown => break,
+            }
+        }
+    };
+
+    tokio::select! {
+        _ = reader_task => {},
+        _ = writer_task => {},
+    }
+
+    let _ = event_tx.send(UiEvent::ConnectionLost);
+}
+
+async fn write_packet<W>(writer: &mut W, packet: &ControlPacket) -> std::io::Result<()>
+where
+    W: AsyncWriteExt + Unpin,
+{
+    let payload = encode_packet(packet);
+    writer.write_u32_le(payload.len() as u32).await?;
+    writer.write_all(&payload).await?;
+    writer.flush().await?;
+    Ok(())
+}
+
+async fn read_packet<R>(reader: &mut R) -> std::io::Result<ControlPacket>
+where
+    R: AsyncReadExt + Unpin,
+{
+    let len = reader.read_u32_le().await? as usize;
+    let mut buf = AlignedVec::with_capacity(len);
+    reader.read_exact(&mut buf).await?;
+    Ok(decode_packet(&buf))
+}
